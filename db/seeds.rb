a702f42036fc03f88d6c013f048d3b5d2dfeda4d
rooserik <rooserik@gmail.com>
2016-02-09 22:30:02 +0100

    created a database, and added artists and songs to the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+sinatra=Artist.create({name:"Frank Sinatra", genre:"Easy Listening",rec_company:"Colombia"})
+green_day=Artist.create({name:"Green Day", genre:"Rock",rec_company:"Fantasy Studios"})
+
+world_on_a_string=Song.create({name:"I've got the world on a string", duration:196, artist:sinatra, album:"This is Sinatra!"})
+fly_me_to_the_moon=Song.create({name:"Fly me to the moon", duration:172, artist:sinatra, album:"This is Sinatra!"})
+basket_case=Song.create({name:"Basket Case", duration:182, artist:green_day, album:"Basket Case"})
+when_i_come_around=Song.create({name:"When I come around", duration:212, artist:green_day, album:"When I come around"})
