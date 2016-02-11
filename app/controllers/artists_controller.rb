ff497286844be29c06801f99fcaec6f6dde7503e
rooserik <rooserik@gmail.com>
2016-02-11 12:59:25 +0100

    added songs render list and artist render list

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,7 @@ class ArtistsController < ApplicationController
   def index
     @artists=Artist.all
   end
+  def show
+    @artists=Artist.find(params[:id])
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,7 @@ class SongsController < ApplicationController
   def index
     @songs=Song.all
   end
+  def show
+    @songs=Song.find(params[:id])
+  end
 end
